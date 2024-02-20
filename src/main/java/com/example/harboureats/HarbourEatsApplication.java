1e57d0cc44311f7f40a4fc6d5197aaa40a9f5d36
mohammad.umair <mohammad.umair@talabat.com>
2024-02-20 23:27:48 +0400

    Fix Entity not managed issue

--- src/main/java/com/example/harboureats/HarbourEatsApplication.java
+++ src/main/java/com/example/harboureats/HarbourEatsApplication.java
@@ -2,10 +2,14 @@ package com.example.harboureats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
+@EntityScan("com.example.harboureats.model.*")
+@EnableJpaRepositories("com.example.harboureats.repository.*")
 public class HarbourEatsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HarbourEatsApplication.class, args);
